ee347a5472ec187432629a16d48d4c6920425e7b
Chef El <chefleroybrown@gmail.com>
2020-01-28 03:20:58 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,16 +3,3 @@
 end
 
 # call your method here!
-def hello_t(array)
-  i = 0
- 
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
- 
-  array
-else
-  puts "Hey! No bolck was given!"
-end
-end
